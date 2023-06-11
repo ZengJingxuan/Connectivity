2c4f81ec591d81fea30d3a09c3d1e9cb176005c9
zjx <g>
2023-06-11 23:38:31 +0900

    edited version: almost the same way to process data as matlab

--- zjx_edited_main.py
+++ zjx_edited_main.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-
 ch1_p = r"230102-W3-ch1.csv"
 ch2_p = r"230102-W3-ch2.csv"
 
@@ -24,7 +23,7 @@ co_y1_tar = np.corrcoef(y1_tar.T)
 co_hm_y1_tar = sns.clustermap(co_y1_tar, xticklabels=tar_name_y1, yticklabels=tar_name_y1, col_cluster=False, row_cluster=False, cmap='jet',vmin=-1, vmax=1,cbar_pos=(0.1, 0.595, 0.03, 0.2))
 ## values for correlation map
 v_y1_tar = co_hm_y1_tar.data2d.values
-
+pd.DataFrame(v_y1_tar).to_csv("230102-W3-tar.csv")
 
 ### all neurons
 sns.set(font_scale=0.8)
@@ -32,11 +31,14 @@ ordered_y1, hm_y1 = zpr.cluster(y1)     ### choose True & False (if use optimal_
 ### yticklabel easier to see
 re_name_ordered_y1 = zpr.re_name_all(ordered_y1)
 hm_y1.ax_heatmap.set_yticklabels(re_name_ordered_y1)
-plt.show()
 
-co_eff_y1 = np.corrcoef(y1.T)  # co_eff matrix
-co_y1 = sns.clustermap(co_eff_y1, cmap='jet', vmin=-1, vmax=1, cbar_pos=(0.02, 0.79, 0.03, 0.2))
+
 # heatmap of co_eff matrix
+co_eff_y1 = np.corrcoef(ordered_y1.T)  # co_eff matrix hm聚类方式不改变coeff结果
+co_y1 = sns.clustermap(co_eff_y1, xticklabels=True, yticklabels=True, cmap='jet', vmin=-1, vmax=1, cbar_pos=(0.02, 0.79, 0.03, 0.2), figsize=(6.7, 8))
+co_y1_xnames, co_y1_ynames = zpr.re_name_all_corr(co_y1, ordered_y1)
+co_y1.ax_heatmap.set_yticklabels(co_y1_ynames)
+co_y1.ax_heatmap.set_xticklabels(co_y1_xnames)
 
 
 zpr.histplot(co_eff_y1, label='0102-1DA-W3')
